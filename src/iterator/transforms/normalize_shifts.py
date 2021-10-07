1cbde9df72cf8adf6724c0bd6676c573c1797fff
Felix Thaler <thaler@cscs.ch>
2021-10-07 12:02:43 +0200

    Added comment

--- src/iterator/transforms/normalize_shifts.py
+++ src/iterator/transforms/normalize_shifts.py
@@ -9,10 +9,12 @@ class NormalizeShifts(NodeTranslator):
             isinstance(node.fun, ir.FunCall)
             and isinstance(node.fun.fun, ir.SymRef)
             and node.fun.fun.id == "shift"
+            and node.args
             and isinstance(node.args[0], ir.FunCall)
             and isinstance(node.args[0].fun.fun, ir.SymRef)
             and node.args[0].fun.fun.id == "shift"
         ):
+            # shift(args1...)(shift(args2...)(it)) -> shift(args2..., args1...)(it)
             assert len(node.args) == 1
             return ir.FunCall(
                 fun=ir.FunCall(
