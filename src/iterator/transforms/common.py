b1e7de66b6b69b4fae12c4702dc1d6e2eb8fc164
Felix Thaler <thaler@cscs.ch>
2021-10-07 12:03:37 +0200

    New inlining transforms

--- src/iterator/transforms/common.py
+++ src/iterator/transforms/common.py
@@ -1,9 +1,14 @@
 from iterator.transforms.inline_fundefs import InlineFundefs, PruneUnreferencedFundefs
+from iterator.transforms.inline_lambdas import InlineLambdas
+from iterator.transforms.inline_lifts import InlineLifts
 from iterator.transforms.normalize_shifts import NormalizeShifts
 
 
 def apply_common_transforms(ir):
     ir = InlineFundefs().visit(ir)
     ir = PruneUnreferencedFundefs().visit(ir)
+    ir = NormalizeShifts().visit(ir)
+    ir = InlineLifts().visit(ir)
+    ir = InlineLambdas().visit(ir)
     ir = NormalizeShifts().visit(ir)
     return ir
