136c541053c5ff5d06ffad6daf57cb928472f256
Hannes Vogt <hannes@havogt.de>
2022-10-25 07:35:46 +0000

    scan in fieldop

---
+++ tests/functional_tests/ffront_tests/test_scan.py
@@ -0,0 +1,44 @@
+from functional.common import DimensionKind
+from functional.ffront.decorator import field_operator, program, scan_operator
+from functional.ffront.fbuiltins import (
+    Dimension,
+    Field,
+    FieldOffset,
+    broadcast,
+    float64,
+    int32,
+    int64,
+    max_over,
+    min_over,
+    neighbor_sum,
+    where,
+)
+from functional.iterator.embedded import (
+    NeighborTableOffsetProvider,
+    index_field,
+    np_as_located_field,
+)
+from functional.program_processors.runners import gtfn_cpu, roundtrip
+from functional.program_processors.codegens.gtfn import gtfn_backend
+
+import numpy as np
+
+def test_scan_in_field_op():
+    size = 10
+    IDim = Dimension("I", kind=DimensionKind.HORIZONTAL)
+    KDim = Dimension("K", kind=DimensionKind.VERTICAL)
+    Koff = FieldOffset("Koff", source=KDim, target=(KDim,))
+    inp = np_as_located_field(IDim, KDim)(np.ones((size, size)))
+    out = np_as_located_field(IDim, KDim)(np.zeros((size, size)))
+
+    @scan_operator(axis=KDim,forward=True,init = 0.0)
+    def sum(state: float, k:float, kp:float):
+        return  state+k+kp
+
+    @field_operator
+    def wrapped(inp: Field[[IDim, KDim], float]):
+        return sum(inp, inp(Koff[1]))
+    @program
+    def prog(inp: Field[[IDim, KDim], float],out: Field[[IDim,KDim], float]):
+        wrapped(inp, out=out[:,:-1])
+    print(gtfn_backend.generate(prog.itir))
